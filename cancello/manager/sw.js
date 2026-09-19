6381fcb47017a97e7204ab86415a1ed23b15d28e
crossmatthew40-coder <crossmatthew40@gmail.com>
2026-09-19 22:44:31 +0100

    Refresh Cancello Manager daily rota app

--- cancello/manager/sw.js
+++ cancello/manager/sw.js
@@ -1,4 +1,4 @@
-const CACHE="cancello-manager-v1";
+const CACHE="cancello-manager-v2";
 const SHELL=["/cancello/manager/","/cancello/manager/index.html","/cancello/manager/manifest.webmanifest","/cancello/staff/apple-touch-icon-v13.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
